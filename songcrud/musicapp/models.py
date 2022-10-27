dc7820c4d20bef039367f61fc149298709df9478
Theresah Boateng <theresahb14@gmail.com>
2022-10-27 20:42:45 +0000

    added artiste model

--- songcrud/musicapp/models.py
+++ songcrud/musicapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Artiste(models.Model):
+    first_name = models.CharField(max_length=400)
+    last_name = models.CharField(max_length=400)
+    age = models.IntegerField()
+ 
+   
